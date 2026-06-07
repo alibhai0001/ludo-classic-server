02aeecef50d375b2ec71c6a072cd14385d523024
alibhai0001 <trickyali8302@gmail.com>
2026-06-07 14:39:06 +0530

    Update live server URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 /** Auto-updated by go_online.py */
-window.LUDO_SERVER = 'https://owen-bones-department-kde.trycloudflare.com';
+window.LUDO_SERVER = 'https://weapon-acoustic-kde-ventures.trycloudflare.com';
 window.LUDO_USE_GITHUB = true;
