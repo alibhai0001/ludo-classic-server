2291f0540c4e5ed2958f03fcce9c01ad94e3e904
alibhai0001 <trickyali8302@gmail.com>
2026-06-07 15:23:30 +0530

    Update live server URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 /** Auto-updated by go_online.py */
-window.LUDO_SERVER = 'https://neutral-daughter-genres-able.trycloudflare.com';
+window.LUDO_SERVER = 'https://trends-triumph-handmade-log.trycloudflare.com';
 window.LUDO_USE_GITHUB = true;
