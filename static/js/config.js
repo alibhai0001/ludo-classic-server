d57b77e832485683df092ad6e67aafb2c6bae871
alibhai0001 <trickyali8302@gmail.com>
2026-06-07 15:25:12 +0530

    Update live server URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 /** Auto-updated by go_online.py */
-window.LUDO_SERVER = 'https://club-lawsuit-submit-overnight.trycloudflare.com';
+window.LUDO_SERVER = 'https://jacksonville-secretariat-constructed-visit.trycloudflare.com';
 window.LUDO_USE_GITHUB = true;
