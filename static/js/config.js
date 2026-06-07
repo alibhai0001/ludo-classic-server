73313ac46d0a6e4b01e9c1a860294a270c5b65d6
alibhai0001 <trickyali8302@gmail.com>
2026-06-07 14:31:41 +0530

    Update live server URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 /** Auto-updated by go_online.py */
-window.LUDO_SERVER = 'https://campus-circus-liabilities-suggesting.trycloudflare.com';
+window.LUDO_SERVER = 'https://owen-bones-department-kde.trycloudflare.com';
 window.LUDO_USE_GITHUB = true;
