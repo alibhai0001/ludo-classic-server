7813a5fc799be2df54ab713a728ef80ee7bf0571
alibhai0001 <trickyali8302@gmail.com>
2026-06-07 14:24:00 +0530

    Update live server URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 /** Auto-updated by go_online.py */
-window.LUDO_SERVER = 'https://pierce-entertaining-stability-capital.trycloudflare.com';
+window.LUDO_SERVER = 'https://leasing-passport-shaft-alien.trycloudflare.com';
 window.LUDO_USE_GITHUB = true;
