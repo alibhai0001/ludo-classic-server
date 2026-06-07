fb41c69753386ce8fe431c064bbfd1d72a643f81
alibhai0001 <trickyali8302@gmail.com>
2026-06-07 15:23:57 +0530

    Update live server URL

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 /** Auto-updated by go_online.py */
-window.LUDO_SERVER = 'https://trends-triumph-handmade-log.trycloudflare.com';
+window.LUDO_SERVER = 'https://club-lawsuit-submit-overnight.trycloudflare.com';
 window.LUDO_USE_GITHUB = true;
